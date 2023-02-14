113436e7188fef80b35bc5738a93219b128b3018
beomwon <38881094+beomwon@users.noreply.github.com>
2023-02-14 23:44:45 +0900

    [level 0] Title: 팩토리얼, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120848. 팩토리얼/팩토리얼.py
+++ 프로그래머스/lv0/120848. 팩토리얼/팩토리얼.py
@@ -1,8 +1,7 @@
 def solution(n):
-    for i in range(1,14):
+    for i in range(1,12):
         num = 1
         for j in range(1,i+1):
             num*=j
         if num > n:
             return i-1  
-            break
