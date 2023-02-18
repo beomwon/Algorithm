f4e1caa212632bc09678025f83dc339033f42338
beomwon <38881094+beomwon@users.noreply.github.com>
2023-02-18 20:42:57 +0900

    [unrated] Title: 카드 뭉치, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.py
@@ -0,0 +1,12 @@
+def solution(cards1, cards2, goal):
+    i1=0
+    i2=0
+    for v in goal:
+        
+        if i1 < len(cards1) and v == cards1[i1]:
+            i1 += 1
+        elif i2 < len(cards2) and v == cards2[i2]:
+            i2 += 1
+        else:
+            return 'No'
+    return 'Yes'
