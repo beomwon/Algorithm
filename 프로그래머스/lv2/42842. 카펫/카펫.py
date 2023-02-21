5c009211b233b86605dd4131b87c5eed306d52f8
이범원 ( Beomwon Lee ) <38881094+beomwon@users.noreply.github.com>
2023-02-21 14:52:13 +0900

    [level 2] Title: 카펫, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42842. 카펫/카펫.py
+++ 프로그래머스/lv2/42842. 카펫/카펫.py
@@ -3,5 +3,5 @@ def solution(b, y):
     # (w-2)*(h-2) = y
     
     for i in range(3, b+y):
-        if (b+y)%i==0 and ((i-2)*((b+y)/i -2)) == y:
+        if ((i-2)*((b+y)/i -2)) == y:
             return sorted([i, (b+y)/i], reverse=True)
