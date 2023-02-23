e27df336f8473a7c7a668b3068f3b14add88136b
이범원 <38881094+beomwon@users.noreply.github.com>
2023-02-23 10:58:18 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.70 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.py
+++ 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.py
@@ -1,7 +1,5 @@
 def solution(n):
-    if n == 1: return 1
-
-    answer = [1, 2]
-    for i in range(2, n):
+    answer = [0, 1, 1]
+    for i in range(1, n):
         answer.append(sum(answer[-2:]))
     return answer[-1]%1234567
