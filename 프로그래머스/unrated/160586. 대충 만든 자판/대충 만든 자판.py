4289cad5b2a861e7c858da8f1fef0dc10c93c487
이범원 <38881094+beomwon@users.noreply.github.com>
2023-02-24 23:25:06 +0900

    [unrated] Title: 대충 만든 자판, Time: 1.26 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/160586. 대충 만든 자판/대충 만든 자판.py
@@ -0,0 +1,16 @@
+def solution(keymap, targets):
+    answer = []
+    kv = {}
+    for keys in keymap:
+        for index, key in enumerate(keys):
+            if key in kv.keys():
+                kv[key] = min(index+1, kv[key])
+            else:
+                kv[key] = index+1
+                
+    for target in targets:
+        try:
+            answer.append(sum([kv[key] for key in target]))
+        except:
+            answer.append(-1)
+    return answer
