57debd9c81819b69d0c377971de0491df5d7a2ca
이범원 <38881094+beomwon@users.noreply.github.com>
2023-03-01 22:55:35 +0900

    [level 2] Title: 스킬트리, Time: 0.06 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/lv2/49993. 스킬트리/스킬트리.py
+++ 프로그래머스/lv2/49993. 스킬트리/스킬트리.py
@@ -2,9 +2,6 @@ import re
 def solution(skill, skill_trees):
     answer = 0
     for s in skill_trees:
-        able = True
-        for i, step in enumerate(re.findall(f'[{skill}]', s)):
-               if skill[i] != step:
-                    able = False
-        answer += int(able)
+        s = ''.join(re.findall(f'[{skill}]', s))
+        answer += int(s == skill[:len(s)])
     return answer
