81ec37a54db778b6f8889aa34ee3be3aca73128a
이범원 <38881094+beomwon@users.noreply.github.com>
2023-04-21 15:26:10 +0900

    [unrated] Title: 과일 장수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/135808. 과일 장수/과일 장수.py
+++ 프로그래머스/unrated/135808. 과일 장수/과일 장수.py
@@ -1,6 +1,3 @@
 def solution(k, m, score):
     score.sort(reverse=True)
-    answer = 0
-    for target in range(m-1, len(score), m):
-        answer += score[target]*m
-    return answer
+    return sum([score[target]*m for target in range(m-1, len(score), m)])
