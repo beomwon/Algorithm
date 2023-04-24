387ffe1a90d7c55c0b193b82dd0021d4def8e6a3
이범원 <38881094+beomwon@users.noreply.github.com>
2023-04-24 14:52:52 +0900

    [unrated] Title: 주사위 게임 3, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181916. 주사위 게임 3/주사위 게임 3.py
+++ 프로그래머스/unrated/181916. 주사위 게임 3/주사위 게임 3.py
@@ -1,8 +1,3 @@
 def solution(a, b, c, d):
-    sets = sorted([[k, [a,b,c,d].count(k)] for k in set([a,b,c,d])], key=lambda x:-x[1])
-    
-    if sets[0][1] == 4: return 1111*sets[0][0]
-    elif sets[0][1] == 3: return (10*sets[0][0] + sets[1][0])**2
-    elif sets[0][1] == 2 and sets[1][1] == 2: return (sets[0][0]+sets[1][0])*((sets[0][0]-sets[1][0])**2)**0.5
-    elif sets[0][1] == 2 and sets[1][1] == 1: return sets[1][0]*sets[2][0]
-    else: return min([a,b,c,d])
+    answer = 0
+    return answer
