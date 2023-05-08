876b2d106dd6065307b7ba4690868871d38ee79b
이범원 <38881094+beomwon@users.noreply.github.com>
2023-05-08 11:40:43 +0900

    [level 2] Title: n^2 배열 자르기, Time: 37.31 ms, Memory: 18.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/87390. n＾2 배열 자르기/n＾2 배열 자르기.py
@@ -0,0 +1,6 @@
+solution=lambda n,l,r:[int(i%n)+1 if int(i/n) < int(i%n) else int(i/n)+1 for i in range(l,r+1)]
+    
+# def solution(n, left, right):
+#     nums = [str(v) for v in range(1,n+1)]
+#     flatten = [str(v)*(v-1) + ''.join(nums[(v-1):]) for v in range(1, n+1)]
+#     return list(map(int,''.join(flatten)))[left:right+1]
