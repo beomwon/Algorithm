2c8e6abfb87604ef4e94c9ecbad42b56ae9772f0
이범원 <38881094+beomwon@users.noreply.github.com>
2023-05-16 17:18:07 +0900

    [level 2] Title: 숫자 블록, Time: 1392.94 ms, Memory: 10.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12923. 숫자 블록/숫자 블록.py
@@ -0,0 +1,20 @@
+def solution(s, e):
+    answer = []
+    if s == 1:
+        answer.append(0)
+        s = s + 1
+        
+    for i in range(s, e+1):
+        n, isadd = [1], True
+        for d in range(2, min(int(i**0.5)+1, 10000000)):
+            if i%d == 0:
+                print(d)
+                n.append(d)
+                if i/d <= 10000000:
+                    answer.append(int(i/d))
+                    isadd = False
+                    break
+        if isadd:
+            answer.append(n[-1])
+            
+    return answer
