047d3014ea4499e28dd06ea39e16a18fd0a4f7b5
이범원 <38881094+beomwon@users.noreply.github.com>
2023-05-17 14:55:40 +0900

    [unrated] Title: 요격 시스템, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181188. 요격 시스템/요격 시스템.py
+++ 프로그래머스/unrated/181188. 요격 시스템/요격 시스템.py
@@ -1,13 +1,12 @@
 def solution(t):
-    t.sort(key=lambda x: x[0])
     lines, overlap = 0, {'s': 0, 'e': 0}
     
-    for s, e in t:
+    
+    for s, e in sorted(t):
         if overlap['e'] <= s:
             lines += 1
             overlap = {'s': s, 'e': e}
         else:
             overlap = {'s': max(overlap['s'], s), 'e': min(overlap['e'], e)}
-
             
     return lines
