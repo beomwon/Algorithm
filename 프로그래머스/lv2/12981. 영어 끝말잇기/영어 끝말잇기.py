a14c2eb2bac96618ebf1b28f0d5dd969918cd5be
이범원 <38881094+beomwon@users.noreply.github.com>
2023-05-19 15:22:25 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.10 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.py
+++ 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.py
@@ -1,8 +1,5 @@
 def solution(n, words):
-    answer = [0, 0]
-    
     for i in range(1, len(words)):
-        if words[i-1][-1] != words[i][0]: return [i%n + 1, int(i/n) + 1]
-        if words[i] in words[:i]: return [i%n + 1, int(i/n) + 1]
-        
+        if words[i-1][-1] != words[i][0] or words[i] in words[:i]:
+            return [i%n+1, i//n+1]
     return [0, 0]
