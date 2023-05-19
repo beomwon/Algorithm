fe016064724d90043bdaa35ffb8dead02f4d701d
이범원 <38881094+beomwon@users.noreply.github.com>
2023-05-19 13:40:27 +0900

    [level 2] Title: 2개 이하로 다른 비트, Time: 236.92 ms, Memory: 23.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/77885. 2개 이하로 다른 비트/2개 이하로 다른 비트.py
@@ -0,0 +1,29 @@
+def solution(numbers):
+    answer = []
+    for num in numbers:
+        r = ('0'+bin(num)[2:])[::-1]
+        found = {'index': len(r), 'what': None}
+        
+        for before in ['00','10','01']:
+            if before in r and r.index(before) < found['index']:
+                found = {'index': r.index(before), 'what': before}
+                
+        change = {'00':'10', '10':'01', '01':'11'} 
+        r = r.replace(found['what'], change[found['what']], 1)
+        
+        if r[-1] == '0':
+            r = r[:-1]
+        answer.append(int(r[::-1],2))
+            
+    return answer
+
+# 00 -> 01
+# 01 -> 10
+# 10 -> 11
+
+# 00 -> 10
+# 10 -> 01
+# 01 -> 11
+
+
+
