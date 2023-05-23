c9bd64a05b31b3352a7aecf03a87dac7fc41ae82
이범원 <38881094+beomwon@users.noreply.github.com>
2023-05-23 09:31:48 +0900

    [unrated] Title: 요격 시스템, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181188. 요격 시스템/요격 시스템.py
+++ 프로그래머스/unrated/181188. 요격 시스템/요격 시스템.py
@@ -1,11 +1,11 @@
 def solution(t):
-    lines, overlap = 0, {'s': 0, 'e': 0}
+    missiles, end = 0, 0
     
     for s, e in sorted(t):
-        if overlap['e'] <= s:
-            lines += 1
-            overlap = {'s': s, 'e': e}
-        else:
-            overlap = {'s': max(overlap['s'], s), 'e': min(overlap['e'], e)}
-            
-    return lines
+        if end <= s:
+            missiles += 1
+            end = e
+        else: 
+            end =  min(end, e)
+
+    return missiles
