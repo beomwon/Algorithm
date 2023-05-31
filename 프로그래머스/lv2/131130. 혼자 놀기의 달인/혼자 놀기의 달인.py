a5b6e8c841ca380a028845ecf04db157cc798c3f
이범원 <38881094+beomwon@users.noreply.github.com>
2023-05-31 09:20:07 +0900

    [level 2] Title: 혼자 놀기의 달인, Time: 0.05 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/131130. 혼자 놀기의 달인/혼자 놀기의 달인.py
@@ -0,0 +1,22 @@
+def solution(cards):
+    boxs = [0]
+    cur, index, count = 0, 0, 0
+    while index < len(cards):
+        if cards[index] == -1:
+            index += 1
+            
+        if cards[cur] == -1:
+            cur = index
+            boxs.append(count)
+            count = 0
+        else:
+            temp = cards[cur]
+            cards[cur] = -1
+            cur = temp - 1
+            count += 1
+            
+        
+            
+    print(boxs)
+    boxs.sort()
+    return boxs[-1]*boxs[-2]
