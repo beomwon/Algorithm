59cb14c3398be3da77ace5bf88834427c9e43428
이범원 <38881094+beomwon@users.noreply.github.com>
2023-06-02 17:29:38 +0900

    [unrated] Title: 옹알이 (2), Time: 0.28 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.js
@@ -0,0 +1,21 @@
+function solution(babbling) {
+    let answer = 0;
+    for (let i = 0; i < babbling.length; i++) {
+        let b = babbling[i];
+        let ableList = ['aya', 'ye', 'woo', 'ma'];
+        for (let j = 0; j < ableList.length; j++) {
+            let able = ableList[j];
+            if (b.indexOf(able.repeat(2)) === -1) {
+                b = b.replace(new RegExp(able, 'g'), ' ');
+            }
+        }
+        if (b.replace(/ /g, '') === '') {
+            answer += 1;
+        }
+    }
+    return answer;
+}
+
+
+
+
