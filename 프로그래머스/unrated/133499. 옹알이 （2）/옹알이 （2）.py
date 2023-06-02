706f15e2e5267fd066051c05afcba6a2f3c8e3af
이범원 <38881094+beomwon@users.noreply.github.com>
2023-06-02 17:13:18 +0900

    [unrated] Title: 옹알이 (2), Time: 0.07 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.py
+++ 프로그래머스/unrated/133499. 옹알이 （2）/옹알이 （2）.py
@@ -9,11 +9,10 @@ def solution(babbling):
                 flag = False
                 break
         if flag: answer.append(v)
-    
+
     for v in answer:
         v = v.replace('aya','A').replace('ye','Y').replace('woo','W').replace('ma','M')
         v = v.replace('A','').replace('Y','').replace('W','').replace('M','')
-        # print(v)
         if v == '': count += 1
-        
+
     return count
