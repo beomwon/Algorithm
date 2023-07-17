72c87bc84beeb2083533358a0a4f14cff360be0c
이범원 <38881094+beomwon@users.noreply.github.com>
2023-07-17 14:25:36 +0900

    [unrated] Title: 둘만의 암호, Time: 0.04 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.py
+++ 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.py
@@ -1,6 +1,6 @@
 def solution(s, skip, index):
     answer = ''
-    alphabet_list = [askii_num for askii_num in range(97,97+26) if askii_num not in [ord(skip_word) for skip_word in skip]] * index
+    alphabet = [askii_num for askii_num in range(97,97+26) if askii_num not in [ord(skip_word) for skip_word in skip]] * index
     for w in s:
-        answer += chr(alphabet_list[alphabet_list.index(ord(w)) + index])
+        answer += chr(alphabet[alphabet.index(ord(w)) + index])
     return answer
