56592ab7e5806638981d490ea389ca7d9caaf825
이범원 <38881094+beomwon@users.noreply.github.com>
2023-07-17 16:21:03 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.py
@@ -1,46 +1,9 @@
 def solution(s):
-    i = 0
-    ans = ''
+    i, ans = 0, ''
+    
     for w in s:
-        if w == ' ':
-            i = 0
-            ans += w
-        else:
-            if i%2 == 0:
-                ans += w.upper()
-            else:
-                ans += w.lower()
-            i += 1
-    return ans
-            
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# def solution(s):
-#     answer = ''
-#     flag = True
-#     for i in range(len(s)):
-#         if s[i] == ' ':
-#             answer+= s[i]
-#             flag = False
-#         else:
-#             if flag:
-#                 answer+= s[i].upper()
-#             else:
-#                 answer+= s[i].lower()
-#         flag = not flag
+        if i%2 == 0: ans += w.upper()
+        else: ans += w.lower()
+        i = i + 1 if w != ' ' else 0
         
-#     return answer
+    return ans
