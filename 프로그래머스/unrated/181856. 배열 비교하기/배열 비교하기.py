ca178f2b422b331b66ae660463400d3a998f98f6
이범원 <38881094+beomwon@users.noreply.github.com>
2023-08-03 17:02:35 +0900

    [unrated] Title: 배열 비교하기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181856. 배열 비교하기/배열 비교하기.py
+++ 프로그래머스/unrated/181856. 배열 비교하기/배열 비교하기.py
@@ -1,6 +1,3 @@
 def solution(arr1, arr2):
-    la, lb = len(arr1), len(arr2)
-    sa, sb = sum(arr1), sum(arr2)
-    if la > lb or (la==lb and sa > sb): return 1
-    if lb > la or (la==lb and sb > sa): return -1
-    return 0
+    answer = 0
+    return answer
