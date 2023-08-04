cbec5638b2cb64cb8f59665ec6c80a220e33be49
이범원 <38881094+beomwon@users.noreply.github.com>
2023-08-04 15:46:11 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.py
+++ 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.py
@@ -1,18 +1,30 @@
 def solution(id_list, report, k):
-    result = []
-    info = {key: {'report': [], 'count': 0} for key in id_list}
+    report = set(report)
+    t = [v.split()[1] for v in report]
+    r = [v for v in id_list if k<=t.count(v)]
+    answer = [0]*len(id_list)
     
-    for history in set(report):
-        reporter, offender = history.split()
-        info[reporter]['report'].append(offender)
-        info[offender]['count'] += 1
+    for v in report:
+        a, b = v.split()
+        answer[id_list.index(a)] += int(b in r)
     
-    for user in id_list:
-        count = 0
-        for offender in info[user]['report']:
-            if k <= info[offender]['count']:
-                count += 1
+    return answer
+            
+# def solution(id_list, report, k):
+#     result = []
+#     info = {key: {'report': [], 'count': 0} for key in id_list}
+    
+#     for history in set(report):
+#         reporter, offender = history.split()
+#         info[reporter]['report'].append(offender)
+#         info[offender]['count'] += 1
+    
+#     for user in id_list:
+#         count = 0
+#         for offender in info[user]['report']:
+#             if k <= info[offender]['count']:
+#                 count += 1
         
-        result.append(count)
+#         result.append(count)
         
-    return result
+#     return result
