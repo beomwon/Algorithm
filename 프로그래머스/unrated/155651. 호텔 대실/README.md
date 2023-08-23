bea748c33c1658962b33f742efe52c40c4b868c2
이범원 <38881094+beomwon@users.noreply.github.com>
2023-08-23 09:27:39 +0900

    [unrated] Title: 호텔 대실, Time: 2.01 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/unrated/155651. 호텔 대실/README.md
+++ 프로그래머스/unrated/155651. 호텔 대실/README.md
@@ -1,10 +1,10 @@
 # [unrated] 호텔 대실 - 155651 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/155651#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/155651) 
 
 ### 성능 요약
 
-메모리: 10.4 MB, 시간: 1.85 ms
+메모리: 10.6 MB, 시간: 2.01 ms
 
 ### 구분
 
@@ -12,7 +12,7 @@
 
 ### 채점결과
 
-<br/>정확성: 100.0<br/>합계: 100.0 / 100.0
+Empty
 
 ### 문제 설명
 
--- 프로그래머스/unrated/155651. 호텔 대실/호텔 대실.py
+++ 프로그래머스/unrated/155651. 호텔 대실/호텔 대실.py
@@ -1,18 +1,33 @@
-def time_convert(string) :
-    h, m = map(int, string.split(":"))
-    return h*60 + m
+# def time_convert(string) :
+#     h, m = map(int, string.split(":"))
+#     return h*60 + m
+
+# def solution(book_time):
+#     answer = 0
+#     check_change_list = list()
+#     for start, end in book_time :
+#         check_change_list.append((time_convert(start), 1))
+#         check_change_list.append((time_convert(end)+10, 0))
+
+#     check_change_list.sort()
+
+#     tmp = 0
+#     for t, chk in check_change_list :
+#         tmp += -1 if chk == 0 else 1
+#         answer = max(answer, tmp)
+#     return answer
 
 def solution(book_time):
-    answer = 0
-    check_change_list = list()
-    for start, end in book_time :
-        check_change_list.append((time_convert(start), 1))
-        check_change_list.append((time_convert(end)+10, 0))
+    timeline = [] 
+    for s, e in book_time:
+        sh, sm = map(int, s.split(':'))
+        eh, em = map(int, e.split(':'))
+        timeline += [(sh*60+sm, 1), (eh*60+em+10, 0)]
     
-    check_change_list.sort()
+    answer, room = 0, 0
+    for _, is_check_in in sorted(timeline):
+        room += 1 if is_check_in else -1
+        answer = max(answer, room)
     
-    tmp = 0
-    for t, chk in check_change_list :
-        tmp += -1 if chk == 0 else 1
-        answer = max(answer, tmp)
     return answer
+        
