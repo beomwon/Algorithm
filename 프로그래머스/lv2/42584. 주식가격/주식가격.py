d71cc768f840f59530dcf00f7ac9cea2bc051821
이범원 <38881094+beomwon@users.noreply.github.com>
2023-08-23 12:40:11 +0900

    [level 2] Title: 주식가격, Time: 13.51 ms, Memory: 16.9 MB -BaekjoonHub

--- 프로그래머스/lv2/42584. 주식가격/주식가격.py
+++ 프로그래머스/lv2/42584. 주식가격/주식가격.py
@@ -1,9 +1,17 @@
 def solution(prices):
     answer = [0]*len(prices)
+    stack = []
     
-    for i in range(len(prices)):
-        for j in range(i+1, len(prices)):
-            answer[i] += 1
-            if prices[j] < prices[i]:
-                break
+    for i, price in enumerate(prices):
+        #stack이 비었이면 false
+        while stack and price < prices[stack[-1]]:
+            j = stack.pop()
+            answer[j] = i - j
+        stack.append(i)
+
+    # for문 다 돌고 Stack에 남아있는 값들 pop
+    while stack:
+        j = stack.pop()
+        answer[j] = len(prices) - 1 - j
+
     return answer
