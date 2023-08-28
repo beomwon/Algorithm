053de06abedfd2e6cde265bceb8785f906dcb085
이범원 <38881094+beomwon@users.noreply.github.com>
2023-08-28 15:12:16 +0900

    [level 2] Title: 주식가격, Time: 13.65 ms, Memory: 16.8 MB -BaekjoonHub

--- 프로그래머스/lv2/42584. 주식가격/주식가격.py
+++ 프로그래머스/lv2/42584. 주식가격/주식가격.py
@@ -3,13 +3,11 @@ def solution(prices):
     stack = []
     
     for i, price in enumerate(prices):
-        #stack이 비었이면 false
         while stack and price < prices[stack[-1]]:
             j = stack.pop()
             answer[j] = i - j
         stack.append(i)
 
-    # for문 다 돌고 Stack에 남아있는 값들 pop
     while stack:
         j = stack.pop()
         answer[j] = len(prices) - 1 - j
