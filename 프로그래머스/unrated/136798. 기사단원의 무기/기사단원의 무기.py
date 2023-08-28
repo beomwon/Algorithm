c722f7f6f7dc7bcd40493c9314517b1e7eec8f45
이범원 <38881094+beomwon@users.noreply.github.com>
2023-08-28 17:01:10 +0900

    [unrated] Title: 기사단원의 무기, Time: 0.73 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.py
+++ 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.py
@@ -1,18 +1,28 @@
 def solution(number, limit, power):
-    k = [1 for _ in range(number+1)]
-    c = [0 for _ in range(number+1)]
-    for i in range(len(k)):
-        if i == 0:
-            continue
-        for j in range(i,len(k),i):
-            k[j] *= i
-            c[j] += 1
-    c = c[1:]
+    weapon = [0]*(number+1)
     answer = 0
-    for i in c:
-        if i <= limit:
-            answer += i
-        else:
-            answer += power
-
+    for i in range(1, len(weapon)):
+        for j in range(i, len(weapon), i):
+            weapon[j] += 1
+    for v in weapon[1:]:
+        answer += v if v <= limit else power
+        
     return answer
+# def solution(number, limit, power):
+#     k = [1 for _ in range(number+1)]
+#     c = [0 for _ in range(number+1)]
+#     for i in range(len(k)):
+#         if i == 0:
+#             continue
+#         for j in range(i,len(k),i):
+#             k[j] *= i
+#             c[j] += 1
+#     c = c[1:]
+#     answer = 0
+#     for i in c:
+#         if i <= limit:
+#             answer += i
+#         else:
+#             answer += power
+
+#     return answer
