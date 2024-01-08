2532649bbfe0337e8c745ed28cefd65a69c84e57
이범원 <38881094+beomwon@users.noreply.github.com>
2024-01-08 11:35:31 +0900

    [level unrated] Title: 가장 많이 받은 선물, Time: 4.24 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/unrated/258712. 가장 많이 받은 선물/가장 많이 받은 선물.py
+++ 프로그래머스/unrated/258712. 가장 많이 받은 선물/가장 많이 받은 선물.py
@@ -1,19 +1,19 @@
 def solution(friends, gifts):
-    gift_info = {name: {'friends': {k: 0 for k in friends}, 'score': 0} for name in friends}
+    history = {name: {'friends': {k: 0 for k in friends}, 'score': 0} for name in friends}
     next_month_gift = {name: 0 for name in friends}
     
     for gift in gifts:
         give, get = gift.split()
         
-        gift_info[give]['friends'][get] += 1
-        gift_info[give]['score'] += 1
+        history[give]['friends'][get] += 1
+        history[give]['score'] += 1
         
-        gift_info[get]['friends'][give] -= 1
-        gift_info[get]['score'] -= 1
+        history[get]['friends'][give] -= 1
+        history[get]['score'] -= 1
     
-    for name in gift_info.keys():
-        for friend_name, friend_score in gift_info[name]['friends'].items():
-            if friend_score > 0 or (friend_score == 0 and gift_info[name]['score'] > gift_info[friend_name]['score']):
+    for name in history.keys():
+        for fname, fscore in history[name]['friends'].items():
+            if fscore > 0 or (fscore == 0 and history[name]['score'] > history[fname]['score']):
                 next_month_gift[name] += 1
 
     return max(next_month_gift.values())
