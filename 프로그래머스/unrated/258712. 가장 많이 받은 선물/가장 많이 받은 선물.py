0861fa3a789b20b78b3c09b279dd6f05a5cb8003
이범원 <38881094+beomwon@users.noreply.github.com>
2024-01-08 11:31:57 +0900

    [level unrated] Title: 가장 많이 받은 선물, Time: 1.96 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/unrated/258712. 가장 많이 받은 선물/가장 많이 받은 선물.py
+++ 프로그래머스/unrated/258712. 가장 많이 받은 선물/가장 많이 받은 선물.py
@@ -1,5 +1,7 @@
 def solution(friends, gifts):
-    gift_info = {name: {'friends': {k: 0 for k in friends}, 'score': 0, 'total': 0} for name in friends}
+    gift_info = {name: {'friends': {k: 0 for k in friends}, 'score': 0} for name in friends}
+    next_month_gift = {name: 0 for name in friends}
+    
     for gift in gifts:
         give, get = gift.split()
         
@@ -12,13 +14,6 @@ def solution(friends, gifts):
     for name in gift_info.keys():
         for friend_name, friend_score in gift_info[name]['friends'].items():
             if friend_score > 0 or (friend_score == 0 and gift_info[name]['score'] > gift_info[friend_name]['score']):
-                print(name, friend_name)
-                gift_info[name]['total'] += 1
-    
-    answer = 0
-    for k, v in gift_info.items():
-        answer = max(answer, v['total'])
-    
-    print(gift_info)
-        
-    return answer
+                next_month_gift[name] += 1
+
+    return max(next_month_gift.values())
