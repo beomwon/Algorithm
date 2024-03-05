dda52aba5d3568e5e1a57c0725b55c57e7daeeec
이범원 <38881094+beomwon@users.noreply.github.com>
2024-03-05 17:26:32 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 120 ms, Memory: 9308 KB -BaekjoonHub

--- 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.js
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.js
@@ -1,4 +1,3 @@
-const fs = require('fs');
-const input = Number(fs.readFileSync("/dev/stdin").toString().trim());
+const input = Number(require('fs').readFileSync("/dev/stdin").toString().trim());
 process.stdout.write('long '.repeat(parseInt(input/4)));
 console.log('int');
