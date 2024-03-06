2c896bce1f6bc15ef1bb3cdc1c43d892fc2b1bed
이범원 <38881094+beomwon@users.noreply.github.com>
2024-03-06 15:07:06 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 붕대 감기, Time: 0.50 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.py
+++ 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.py
@@ -1,19 +1,20 @@
 def solution(bandage, health, attacks):
-    count = 0
-    max_health = health
-    for time in range(attacks[-1][0]+1):
-        if time == attacks[0][0]:
+    casting_second, current_health, end = 0, health, attacks[-1][0]+1
+    casting_success, heal, additional_heal = bandage
+    
+    for time in range(end):
+        if time == attacks[0][0]: # 현재시간에 공격이 들어오면
             _, damage = attacks.pop(0)
-            health -= damage
-            count = 0
-            if health <= 0:
+            current_health -= damage
+            casting_second = 0
+            if current_health <= 0:
                 return -1
-        else:
-            count += 1
-            if count%bandage[0] == 0:
-                count = 0
-                health += bandage[2]
-            health += bandage[1]
-            health = min(max_health, health)
+        else: # 공격이 들어오지 않으면
+            casting_second += 1
+            if casting_second == casting_success:
+                casting_second = 0
+                current_health += additional_heal
+            current_health += heal
+            current_health = min(health, current_health)
             
-    return health
+    return current_health
