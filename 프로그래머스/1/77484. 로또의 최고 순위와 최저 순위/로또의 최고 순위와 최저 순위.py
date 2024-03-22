23e502914816741315b09a30e1690acf7d17dd59
이범원 <38881094+beomwon@users.noreply.github.com>
2024-03-22 15:03:59 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.py
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.py
@@ -1,4 +1,5 @@
 def solution(lottos, win_nums):
+    rank = [6, 6, 5, 4, 3, 2, 1]
     alpha = lottos.count(0)
-    basic = 7 - sum(lottos.count(v) for v in win_nums)
-    return [min(max(1, basic-alpha),6), min(6, basic)]
+    basic = sum(lottos.count(v) for v in win_nums)
+    return [rank[alpha+basic], rank[basic]]
